7a14ec2fbc8a1741d7d7df85910d6a7992acb183
Chris Barber-Riley <chris@aceville.co.uk>
2018-01-15 16:53:50 +0000

    Firing off a dispatch or two

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -3,7 +3,13 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from '../store/storeIndex'
 
+import { addTodo } from '../actions/actionsIndex'
+
 const store = configureStore();
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(addTodo({text: 'Some text in here' }))
+store.dispatch(addTodo({text: 'Other text' }))
 
 const Jsx = (props) => (
   <Provider store={store}>
